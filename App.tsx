8ea23b9bd11ddf4411d3314a4d393dc16f6e32e0
Felipe Diniz <paladiows@gmail.com>
2021-07-15 00:11:57 -0300

    Componente de Input

--- App.tsx
+++ App.tsx
@@ -10,7 +10,9 @@ import {
 } from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
+
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -24,7 +26,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
 
   )
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+} from './styles'
+
+import { Input } from '../../components/Form/Input';
+
+export function Register() {
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+
+      <Form>
+        <Input
+          placeholder="Nome"
+        />
+        <Input
+          placeholder="Preço"
+        />
+      </Form>
+
+    </Container>
+  )
+}
