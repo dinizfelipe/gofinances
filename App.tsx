bca4d901a4e4abf0201c4b7e0f62edbb36b60710
Felipe Diniz <paladiows@gmail.com>
2021-07-21 00:03:56 -0300

    Estilização da Interface para selecionar Categoria

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import {
 } from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 
 export default function App() {
@@ -26,7 +26,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <CategorySelect />
     </ThemeProvider>
 
   )
